448d878eef32408c0bb5f7f8db656fc43a889307
Vladimir Kravchenko <Vladimir.Kravchenko@playtech.com>
2016-10-06 19:16:29 +0300

    Session_0002/Task_0002 done!

---
+++ Homework/Session_0002/task_0002/common.js
@@ -0,0 +1,17 @@
+(function (win) {
+    function showParams() {
+        for(item of arguments) {
+            console.log(item);
+        }
+    };
+    
+    var x=y=z=0;
+    
+    showParams(x,y,z);
+    
+    x=1;
+    y=2;
+    z=3;
+
+    showParams(x,y,z);
+})();
